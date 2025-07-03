21b80ebff57e2f5d6c496734d50df1c13b3612e1
Rashel-Miah <rashelcs@yahoo.com>
2025-07-03 11:49:55 +0600

    'login_chat'

--- settings/db.py
+++ settings/db.py
@@ -6,7 +6,6 @@ def get_connection():
     #return oracledb.connect(**st.secrets.db_credentials)
     #return oracledb.connect(user=st.secrets.db_credentials.user, password=st.secrets.db_credentials.password,dsn=st.secrets.db_credentials.dsn)
     return cx_Oracle.connect(**st.secrets.db_credentials)
-    # return cx_Oracle.connect("aitest", "aitest", "localhost:1521/orcl.amernitech.com")
 
 # conn = get_connection()
 # cursor = conn.cursor()
